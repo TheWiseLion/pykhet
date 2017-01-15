69d9133ac4d5e332baf7e86ad02498a307b45e86
John Mecham <jon.mecham@gmail.com>
2017-01-15 02:21:40 -0500

    Altered Setup

--- setup.py
+++ setup.py
@@ -4,14 +4,18 @@ setup(
     name='pykhet',
     packages=['pykhet'],  # this must be the same as the name above
     version='0.1',
-    description='A',
+    description='A general library for the board game khet',
     author='John Mecham',
     author_email='jon.mecham@gmail.com',
     url='https://github.com/TheWiseLion/pykhet',  # use the URL to the github repo
     download_url='https://github.com/TheWiseLion/pykhet/tarball/0.1',
     keywords=['khet', 'boardgame', 'khet2.0', 'laser-chess', 'ai'],  # arbitrary keywords
-    classifiers=[],
+    classifiers=['Topic :: Games/Entertainment :: Board Games',
+                 'Programming Language :: Python :: 2.7',
+                 'License :: OSI Approved :: MIT License',
+                 ],
     install_requires=[
           'enum34',
-    ]
+    ],
+
 )
