51945194a9e24b202c98aef17be5dfbb0bec08cc
John Mecham <jon.mecham@gmail.com>
2017-02-17 05:28:16 -0500

    * Still hoping to get this compiled in python 3

--- setup.py
+++ setup.py
@@ -7,12 +7,12 @@ test_module = Extension('khetsearch',
 setup(
     name='pykhet',
     packages=['pykhet','pykhet.components','pykhet.games','pykhet.solvers'],  # this must be the same as the name above
-    version='0.16',
+    version='0.17',
     description='A general library for the board game khet',
     author='John Mecham',
     author_email='jon.mecham@gmail.com',
     url='https://github.com/TheWiseLion/pykhet',  # use the URL to the github repo
-    download_url='https://github.com/TheWiseLion/pykhet/tarball/0.16',
+    download_url='https://github.com/TheWiseLion/pykhet/tarball/0.17',
     keywords=['khet', 'boardgame', 'khet2.0', 'laser-chess', 'ai'],  # arbitrary keywords
     classifiers=['Topic :: Games/Entertainment :: Board Games',
                  'Programming Language :: Python :: 2.7',
